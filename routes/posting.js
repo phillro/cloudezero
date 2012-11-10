7f1f31d5fba2a6acfed425c12cd91d7c21d9e949
Brian Starke <brian.starke@gmail.com>
2012-11-10 12:27:48 -0500

    set up posting routes

--- routes/posting.js
+++ routes/posting.js
@@ -3,8 +3,30 @@ var models = require('../models');
 exports.addPosting = function (req, res) {
   var post = req.body;
 
-  console.log("New posting from " + req.session.user_id);
-  console.log(post.imageUrl);
+  models.user.findById(req.session.user_id, function (err, user) {
+    // check for repost before adding
+    models.posting.count({'imageUrl':imageUrl}, function (err, cnt) {
+      if (cnt === 0) {
+        var newPost = new models.posting;
 
-  res.send("DENIED");
+        newPost.imageUrl = post.imageUrl;
+        newPost.userId = user.id;
+        newPost.nickname = user.nickname;
+        newPost.save(function (err) {
+          if (!err) {
+            user.posts++;
+            user.save();
+            //client.publish('updates', newPost.id);
+            console.log(user.nickname + " added " + newPost.imageUrl);
+            res.send("OK");
+          }
+        });
+      } else {
+        user.reposts++;
+        user.save();
+        console.log(user.nickname + " reposted " + newPost.imageUrl);
+        res.send("REPOST");
+      }
+    });
+  });
 };
