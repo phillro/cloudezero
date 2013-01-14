5f72386b3760eafa0dd6e977bd51f3a3987063fa
Brian Starke <brian.starke@gmail.com>
2013-01-13 23:57:02 -0500

    changed mongo password and removed from config

--- etc/config.js
+++ etc/config.js
@@ -1,21 +1,21 @@
 module.exports = {
-    mongo:{
-        dbName:'heroku_app8902478',
-        port:41347,
-        host:'ds041347.mongolab.com',
-        user:'cloudezero',
-        password:'pancakes'
-    },
-    redis:{
-        url:'redis://redistogo:e178a863c9c44954821e7bb50c317c2b@grouper.redistogo.com:9135/'
-    },
-    email:{
-        server:{
-            user:'bronsteinomatic@gmail.com',
-            password:'cloude000',
-            host:'smtp.gmail.com',
-            domain:'cloudezero.net',
-            ssl:true},
-        from:'J.P. Bronstein <bronsteinomatic@gmail.com>'
-    }
+  mongo:{
+    dbName:'heroku_app8902478',
+    port:41347,
+    host:'ds041347.mongolab.com',
+    user:'cloudezero',
+    password:process.env.MONGO_PASSWORD
+  },
+  redis:{
+    url:'redis://redistogo:e178a863c9c44954821e7bb50c317c2b@grouper.redistogo.com:9135/'
+  },
+  email:{
+    server:{
+      user:'bronsteinomatic@gmail.com',
+      password:'cloude000',
+      host:'smtp.gmail.com',
+      domain:'cloudezero.net',
+      ssl:true},
+    from:'J.P. Bronstein <bronsteinomatic@gmail.com>'
+  }
 };
